5a9900795cabadaafa343f22017ccdfb9dc632d7
Mark A. Hershberger <mah@nichework.com>
2018-03-06 20:24:57 -0500

    XrefPanel is working better
    * Add Container.css
    * Move Xref JS to be in the same file with the rest (in WikiPathways)

--- modules/PathwayLoader.js
+++ modules/PathwayLoader.js
@@ -1,20 +1,3 @@
-if (window.hasOwnProperty("XrefPanel")) {
-	XrefPanel.show = function(elm, id, datasource, species, symbol) {
-		jqelm = $(elm);
-		if(XrefPanel.currentTriggerDialog) {
-			XrefPanel.currentTriggerDialog.dialog("close");
-			XrefPanel.currentTriggerDialog.dialog("destroy");
-		}
-		jqcontent = XrefPanel.create(id, datasource, species, symbol);
-		var x = jqelm.offset().left - $(window).scrollLeft();
-		var y = jqelm.offset().top - $(window).scrollTop();
-		jqdialog = jqcontent.dialog({
-			position: [x,y]
-		});
-		XrefPanel.currentTriggerDialog = jqdialog;
-	}
-}
-
 var pvjsInput = JSON.parse( mw.config.get( "pvjsString" ) );
 pvjsInput.onReady = function() {};
 window.addEventListener('load', function() {
